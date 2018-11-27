24ef3913728ab2b9e512f39175581345b1ac1b87
Nikolay Karev <karev.n@gmail.com>
2018-11-27 22:38:09 +0500

    New coding standard

--- src/index.js
+++ src/index.js
@@ -14,64 +14,64 @@ import {
   pipeP,
   prop,
   view
-} from "ramda"
-const mapIndexed = addIndex(map)
-const allPromises = Promise.all.bind(Promise)
-const resolve = Promise.resolve.bind(Promise)
-const runIn = curry((context, validator) => validator(context))
+} from "ramda";
+const mapIndexed = addIndex(map);
+const allPromises = Promise.all.bind(Promise);
+const resolve = Promise.resolve.bind(Promise);
+const runIn = curry((context, validator) => validator(context));
 const hasError = compose(
   complement(isNil),
   prop("message")
-)
-const objectLens = lensProp("object")
-const getPathProp = prop("path")
+);
+const objectLens = lensProp("object");
+const getPathProp = prop("path");
 
 export const validate = curry(function validate(validators, context) {
   const runAll = pipe(
     map(runIn(context)),
     allPromises
-  )
+  );
   return pipeP(
     runAll,
     flatten,
     filter(hasError)
-  )(validators)
-})
+  )(validators);
+});
 
 const decide = curry(function decide(context, message, valid) {
-  return valid ? context : { ...context, message }
-})
+  return valid ? context : { ...context, message };
+});
 
-const check = curry(function check(func, message, context) {
+export const check = curry(function check(func, message, context) {
   const v = pipe(
     view(objectLens),
-    getPath(context.path),
+    getPath(context.path || []),
     func,
     resolve
-  )
+  );
   return pipeP(
     v,
     decide(context, message)
-  )(context)
-})
+  )(context);
+});
 
-const concatPath = (path, context) => concat(getPathProp(context) || [], path)
+const concatPath = (path, context) => concat(getPathProp(context) || [], path);
 
 export const path = curry(function path(func, message, path, context) {
-  return check(func, message, { ...context, path: concatPath(path, context) })
-})
+  return check(func, message, { ...context, path: concatPath(path, context) });
+});
 
 export const nest = curry(function nest(nestPath, validators, context) {
   return validate(validators, {
     ...context,
     path: concatPath(nestPath, context)
-  })
-})
+  });
+});
 
 export const array = curry(function array(validators, context) {
-  const values = getPath(context.path, context.object)
+  const values = getPath(context.path, context.object);
   if (!values) {
-    return resolve([])
+    return resolve([]);
   }
   return allPromises(
     mapIndexed(
@@ -82,5 +82,5 @@ export const array = curry(function array(validators, context) {
         }),
       values
     )
-  )
-})
+  );
+});
--- test/index.test.js
+++ test/index.test.js
@@ -1,50 +1,62 @@
-import { validate, nest, array, path } from "../src"
-import * as R from "ramda"
-const notPresentPredicate = R.or(R.isNil, R.isEmpty)
-export const present = path(R.complement(notPresentPredicate))
-export const notPresent = path(notPresentPredicate)
+import { validate, nest, array, path, check } from "../src";
+import * as R from "ramda";
+const notPresentPredicate = R.or(R.isNil, R.isEmpty);
+export const present = path(R.complement(notPresentPredicate));
+export const notPresent = path(notPresentPredicate);
 
-const presentPredicate = R.complement(R.or(R.isNil, R.isEmpty))
-const makeAsync = predicate => value => Promise.resolve(predicate(value))
-const presentP = path(makeAsync(presentPredicate))
+const presentPredicate = R.complement(R.or(R.isNil, R.isEmpty));
+const makeAsync = predicate => value => Promise.resolve(predicate(value));
+const presentP = path(makeAsync(presentPredicate));
 
+describe("#check()", () => {
+  const isJediPredicate = person => !!person.isJedi;
+  const isJedi = check(isJediPredicate, "Jedi he is not");
+  it("should return an error if there is any", () =>
+    expect(validate([isJedi], { object: { name: "Jabba" } })).resolves.toEqual([
+      { message: "Jedi he is not", object: { name: "Jabba" } }
+    ]));
+  it("returns no errors if all is OK", () =>
+    expect(
+      validate([isJedi], { object: { name: "Luke", isJedi: true } })
+    ).resolves.toEqual([]));
+});
 describe("#validate()", () => {
   it("Gathers plain errors", () => {
     const validator = [
       object => Promise.resolve({ path: ["foo"], message: "bar", object })
-    ]
+    ];
     return expect(
       validate(validator, { foo: "bar", bar: "foo" })
     ).resolves.toEqual([
       { path: ["foo"], message: "bar", object: { foo: "bar", bar: "foo" } }
-    ])
-  })
+    ]);
+  });
 
   it("Gathers async present validator errors", () => {
-    const validator = [presentP("error", ["foo2"])]
+    const validator = [presentP("error", ["foo2"])];
     return expect(
       validate(validator, { object: { foo: "bar", bar: "foo" } })
     ).resolves.toEqual([
       { path: ["foo2"], message: "error", object: { foo: "bar", bar: "foo" } }
-    ])
-  })
+    ]);
+  });
 
   it("Gathers nested errors", () => {
-    const validator = [nest(["foo"], [presentP("error", ["bar"])])]
+    const validator = [nest(["foo"], [presentP("error", ["bar"])])];
     return expect(validate(validator, { object: {} })).resolves.toEqual([
       { path: ["foo", "bar"], message: "error", object: {} }
-    ])
-  })
-})
+    ]);
+  });
+});
 
 describe("#present()", () => {
   it("should return an error if the field is required", () => {
     expect(present("error", ["foo"], {})).resolves.toEqual({
       path: ["foo"],
       message: "error"
-    })
-  })
-})
+    });
+  });
+});
 
 describe("#nest()", () => {
   it("Add prefix to errors", () => {
@@ -52,44 +64,46 @@ describe("#nest()", () => {
       nest(["bar"], [present("error", ["foo"])], {
         object: {}
       })
-    ).resolves.toEqual([{ path: ["bar", "foo"], message: "error", object: {} }])
-  })
+    ).resolves.toEqual([
+      { path: ["bar", "foo"], message: "error", object: {} }
+    ]);
+  });
 
   it("All ok if there are no errors", () => {
     return expect(
       nest(["bar"], [present("error", ["foo"])], {
         object: { bar: { foo: "bar" } }
       })
-    ).resolves.toEqual([])
-  })
-})
+    ).resolves.toEqual([]);
+  });
+});
 
 describe("Ramda", () => {
   it("Takes paths", () => {
     const obj = {
       a: [{ b: 1 }]
-    }
-    const lens = R.lensPath(["a", 0, "b"])
-    expect(R.view(lens, obj)).toBe(1)
-  })
-})
+    };
+    const lens = R.lensPath(["a", 0, "b"]);
+    expect(R.view(lens, obj)).toBe(1);
+  });
+});
 
 describe("#array()", () => {
   it("add array index to errors", () => {
-    const object = { bar: [{ yo: "fo" }] }
+    const object = { bar: [{ yo: "fo" }] };
     return expect(
       nest(["bar"], [array([present("error", ["foo"])])])({
         object
       })
-    ).resolves.toEqual([{ path: ["bar", 0, "foo"], message: "error", object }])
-  })
+    ).resolves.toEqual([{ path: ["bar", 0, "foo"], message: "error", object }]);
+  });
 
   it("dont add anything if there are no array errors", () => {
-    const object = { bar: [{ foo: true }, { foo: true }] }
+    const object = { bar: [{ foo: true }, { foo: true }] };
     return expect(
       nest(["bar"], [array([present("error", ["foo"])])])({
         object
       })
-    ).resolves.toEqual([])
-  })
-})
+    ).resolves.toEqual([]);
+  });
+});
